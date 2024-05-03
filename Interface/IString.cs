8122cb2653fc28a5b69f234a1375cdb0e8d444c0
Amogh <amogh@paybooks.in>
2024-05-03 21:57:40 +0530

    pushing the code to git

---
+++ Interface/IString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCloneApp.Interface
+{
+    public interface IString
+    {
+        public void stringManipulation(string str);
+    } 
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,51 @@
-﻿public class Program
+﻿using GitCloneApp.Interface;
+using System.Text;
+
+public class Program
 {
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello Git!");
+        IString implement = new ImplementInterafce();
+        Mains mains = new Mains(implement);
+        mains.callStringManipulation("Applied system");
         Console.ReadLine();
+
+
     }
 }
+  
+public class Mains
+{
+    private IString implement;
+    public Mains(IString implementInterafce)
+    {
+        this.implement = implementInterafce; 
+    }
+
+    public void callStringManipulation(string str)
+    { 
+        implement.stringManipulation(str);
+    }
+}
+public class ImplementInterafce : IString
+{
+    public void stringManipulation(string str)
+    {
+
+        //Applied system
+        Console.WriteLine("Out put is mpplied systeA");
+        StringBuilder sb = new StringBuilder();
+        char[] chars = str.ToCharArray();
+
+        sb.Append(chars[13]);
+        for(int i = 0; i < chars.Length; i++)
+        {
+            if (i == 0 || i == 14)
+                continue;
+            sb.Append(chars[i]);
+        }
+        sb.Append(chars[0]);
+        Console.WriteLine("Output is {0}",sb.ToString());
+    } 
+}  
